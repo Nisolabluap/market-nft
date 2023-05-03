ab02c8656f52f49f85f4e711a01f1074eb2cf963
Nisolabluap <balosin.paul@protonmail.com>
2023-05-04 00:53:58 +0300

    creat pagini de navigare pagina globala footer navbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,18 @@
-import '@/styles/globals.css'
+import { ThemeProvider } from 'next-themes';
+import Script from 'next/script';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import { Navbar, Footer } from '../components';
+import '../styles/globals.css';
+
+const MyApp = ({ Component, pageProps }) => (
+  <ThemeProvider attribute="class">
+    <div className="dark:bg-nft-dark bg-white min-h-screen">
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+    <Script src="https://kit.fontawesome.com/c56e8d8e17.js" crossOrigin="anonymous" />
+  </ThemeProvider>
+);
+
+export default MyApp;
